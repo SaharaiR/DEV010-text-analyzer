4d03bcafe11403d1f56529c696887ac7dd725b3d
Sahara <sahararod@gmail.com>
2023-07-24 09:13:16 -0600

    funciones avanzando

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,9 +1,21 @@
-const analyzer = {  
+let contadorCaracter = 0;
+let contadorNumeros = 0;
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //console.log(text);
+    
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    // eslint-disable-next-line no-undef
+    let i = 0;
+    if((text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) || (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122)){
+      contadorCaracter++;
+      i++;
+      return contadorCaracter;
+    }
+    
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
@@ -13,6 +25,14 @@ const analyzer = {
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    /*if((text.charCodeAt(0) >= 48 && text.charCodeAt(0) <= 57)){
+      contadorNumeros++;
+      return contadorNumeros;
+    }*/
+    if(/^\d$/.text){
+      contadorCaracter++;
+      return contadorCaracter;
+    }
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+const textoEscrito = document.querySelector('user-input');//LIGA AL TEXTAREA
+const btnLimpiar = document.getElementById('btnLimpiar');//LIGA AL BOTON PARA REINICIAR METRICAS Y TEXTO
+//const resultadoCaracterContador = analyzer.getCharacterCount(textoEscrito.value);
+
+textoEscrito.addEventListener('keyup', function(){
+  //DETECTA EL TEXTO AL ESCRIBIRLO HAY QUE ENVIAR EL TEXTO A CADA FUNCION
+
+  //Recuento de caracteres: 0; data-testid="character-count
+  const resultadoCaracterContador = analyzer.getCharacterCount(textoEscrito.value);
+  document.querySelector('[data-testid="character-count"]').innerHTML = 'Recuento de caracteres: ' + resultadoCaracterContador;
+
+  analyzer.getCharacterCountExcludingSpaces(textoEscrito.value);
+
+  analyzer.getAverageWordLength(textoEscrito.value);
+
+  //Recuento de números: 0; data-testid="number-count
+  const resultadoContadorNum = analyzer.getNumberCount(textoEscrito.value);
+  document.querySelector('[data-testid="number-count"]').innerHTML = 'Recuento de números: ' + resultadoContadorNum;
+
+  analyzer.getNumberSum(textoEscrito.value);
+});
+
+btnLimpiar.addEventListener('click', function(){
+  //REINICIAR METRICAS Y TEXT AREA
+  textoEscrito.value = " ";
+  document.querySelector('[data-testid="word-count"]').innerHTML = 'Recuento de palabras: 0'
+  document.querySelector('[data-testid="character-count"]').innerHTML = 'Recuento de caracteres: 0';
+  document.querySelector('[data-testid="character-no-spaces-count"]').innerHTML = 'Recuento de caracteres excluyendo espacios y signos: 0';
+  document.querySelector('[data-testid="number-count"]').innerHTML = 'Recuento de números: 0';
+  document.querySelector('[data-testid="number-sum"]').innerHTML = 'Suma total de los números: 0';
+  document.querySelector('[data-testid="word-length-average"]').innerHTML = 'Longitud media de las palabras: 0';
+});
